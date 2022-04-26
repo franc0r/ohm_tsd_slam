19ca65c58656221aaa7a54c56664595536f251a3
Christian Wendt <knueppl@gmx.de>
2022-04-26 19:50:09 +0000

    Create ros2 package (empty).

---
+++ src/slam.cpp
@@ -0,0 +1,22 @@
+/*
+ * slam_node.cpp
+ *
+ *  Created on: Apr 28, 2014
+ *      Author: phil
+ */
+
+// #include "SlamNode.h"
+#include <rclcpp/rclcpp.hpp>
+
+// #include "obcore/base/Logger.h"
+
+int main(int argc, char** argv)
+{
+  // ros::init(argc, argv, "slam_node");
+
+  // LOGMSG_CONF("slamlog.log", obvious::Logger::file_off|obvious::Logger::screen_off, DBG_DEBUG, DBG_ERROR);
+
+  // ohm_tsd_slam::SlamNode slamNode;
+  // slamNode.start();
+}
+
